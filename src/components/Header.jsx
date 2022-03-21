1a5305b10e37cd75d76fae4f13ed43251945b95c
almostdevelopr <minhalam.dev@gmail.com>
2022-03-21 18:25:46 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+	return (
+		<Header>
+			<div className="container">
+				<h2>Feedback UI</h2>
+			</div>
+		</Header>
+	);
+}
+
+export default Header;
