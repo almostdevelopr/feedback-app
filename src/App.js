87a69ecdf0f4a29eeffdd3a3a8d7c24d0ff9561b
almostdevelopr <minhalam.dev@gmail.com>
2022-03-21 18:39:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+
 function App() {
 	return (
 		<>
 			<Header />
 			<div className="container">
-				<h1>My Apsp</h1>
+				<h1>My App</h1>
 			</div>
 		</>
 	);
