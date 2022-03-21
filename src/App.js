222209802e2e0dc6dccf9e8f08d31f4da91978c3
almostdevelopr <minhalam.dev@gmail.com>
2022-03-21 17:32:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 function App() {
+	const title = "Blog Post";
+	const body = "This is my first blog post";
+	const comments = [
+		{ id: 1, text: "Comment one" },
+		{ id: 2, text: "Comment two" },
+		{ id: 3, text: "Comment three" },
+		{ id: 4, text: "Comment four" },
+	];
+
 	return (
-		<>
-			<h1 className="myapp">My App</h1>
-			<p>Henlo</p>
-		</>
+		<div className="container">
+			<h1>{title}</h1>
+			<p>{body}</p>
+			<div className="comments">
+				<h3>Comments ({comments.length})</h3>
+				<ul>
+					{comments.map((comment, index) => (
+						<li key={index}>{comment.text}</li>
+					))}
+				</ul>
+			</div>
+		</div>
 	);
 }
 export default App;
