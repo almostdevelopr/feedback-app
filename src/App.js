9ac62079b90f9e24c53cfe9742254a4cfc651bf7
almostdevelopr <minhalam.dev@gmail.com>
2022-03-21 17:49:35 +0530

    Updates for app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 function App() {
-	const title = "Blog Post";
-	const body = "This is my first blog post";
-	const comments = [
-		{ id: 1, text: "Comment one" },
-		{ id: 2, text: "Comment two" },
-		{ id: 3, text: "Comment three" },
-		{ id: 4, text: "Comment four" },
-	];
-	const isLoading = false;
-	const showComments = true;
-	const commentBlock = (
-		<div className="comments">
-			<h3>Comments ({comments.length})</h3>
-			<ul>
-				{comments.map((comment, index) => (
-					<li key={index}>{comment.text}</li>
-				))}
-			</ul>
-		</div>
-	);
-
-	if (isLoading) return <h1>Loading....</h1>;
-
 	return (
 		<div className="container">
-			<h1>{title}</h1>
-			<p>{body}</p>
-
-			{showComments && commentBlock}
+			<h1>My App</h1>
 		</div>
 	);
 }
