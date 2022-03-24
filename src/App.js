f562c1cac8f3595436749a3510fe428e1ce2a431
almostdevelopr <minhalam.dev@gmail.com>
2022-03-24 23:13:03 +0530

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import Header from "./components/Header";
+import Header from "./components/Header";
 function App() {
 	return (
 		<>
-			{/* <Header text="Hello World" /> */}
+			<Header text="Hello World" />
 			<div className="container">
 				<h1>My App</h1>
 			</div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-// function Header({ text }) {
-// 	return (
-// 		<Header>
-// 			<div className="container">
-// 				<h2>{text}</h2>
-// 			</div>
-// 		</Header>
-// 	);
-// }
-// export default Header;
+function Header({ text }) {
+	return (
+		<Header>
+			<div className="container">
+				<h2>{text}</h2>
+			</div>
+		</Header>
+	);
+}
+export default Header;
