84a9b05a8280173b6849cded964dd10bc5de67ae
almostdevelopr <minhalam.dev@gmail.com>
2022-03-26 00:11:24 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 function Header({ text, bgColor, textColor }) {
 	const headerStyles = {
 		backgroundColor: bgColor,
@@ -16,5 +17,10 @@ Header.defaultProps = {
 	text: "Feedback UI",
 	bgColor: "rgba(0,0,0,0.4)",
 	textColor: "#ff6a95",
+};
+Header.propTypes = {
+	text: PropTypes.string,
+	bgColor: PropTypes.string,
+	textColor: PropTypes.string,
 };
 export default Header;
