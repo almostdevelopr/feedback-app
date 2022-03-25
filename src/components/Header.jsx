a68e1388c48623325e05311a95a39a99700f46c6
almostdevelopr <minhalam.dev@gmail.com>
2022-03-25 23:58:03 +0530

    Add styles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+	const headerStyles = {
+		backgroundColor: bgColor,
+		color: textColor,
+	};
 	return (
-		<Header>
+		<header style={headerStyles}>
 			<div className="container">
 				<h2>{text}</h2>
 			</div>
-		</Header>
+		</header>
 	);
 }
 
 Header.defaultProps = {
 	text: "Feedback UI",
+	bgColor: "rgba(0,0,0,0.4)",
+	textColor: "#ff6a95",
 };
 export default Header;
