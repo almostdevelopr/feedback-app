2883b9159e5b88d654a142f77fab7a2eef8a63b6
almostdevelopr <minhalam.dev@gmail.com>
2022-03-31 17:21:01 +0530

    Add useState hook

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+function FeedbackItem() {
+	const [rating, setRating] = useState(7);
+	const [text, setText] = useState("This is an example of a feedback item");
+
+	const handleClick = () => {
+		setRating(() => {
+			//if want access to previous state/value
+			return 10;
+		});
+	};
+	return (
+		<div className="card">
+			<div className="num-display">{rating}</div>
+			<div className="text-display">{text}</div>
+			<button onClick={handleClick}>Click</button>
+		</div>
+	);
+}
+
+export default FeedbackItem;
