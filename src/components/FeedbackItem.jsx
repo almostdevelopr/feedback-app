c6edcb15414234b05cc72ff60d592023e6e78a1f
almostdevelopr <minhalam.dev@gmail.com>
2022-03-31 17:22:15 +0530

    Update FeedbackItem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,17 +5,17 @@ function FeedbackItem() {
 	const [rating, setRating] = useState(7);
 	const [text, setText] = useState("This is an example of a feedback item");
 
-	const handleClick = () => {
-		setRating((prev) => {
-			//if want access to previous state/value
-			return prev + 1;
-		});
-	};
+	// const handleClick = () => {
+	// 	setRating((prev) => {
+	// 		//if want access to previous state/value
+	// 		return prev + 1;
+	// 	});
+	// };
 	return (
 		<div className="card">
 			<div className="num-display">{rating}</div>
 			<div className="text-display">{text}</div>
-			<button onClick={handleClick}>Click</button>
+			{/* <button onClick={handleClick}>Click</button> */}
 		</div>
 	);
 }
