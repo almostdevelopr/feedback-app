b68d9e8b81c59b9a5475e947d9016f9f75e1460f
almostdevelopr <minhalam.dev@gmail.com>
2022-03-31 18:16:43 +0530

    Add list of cards

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,8 @@
-import React from "react";
-import { useState } from "react";
-
-function FeedbackItem() {
-	const [rating, setRating] = useState(7);
-	const [text, setText] = useState("This is an example of a feedback item");
-
-	// const handleClick = () => {
-	// 	setRating((prev) => {
-	// 		//if want access to previous state/value
-	// 		return prev + 1;
-	// 	});
-	// };
+function FeedbackItem({ item }) {
 	return (
 		<div className="card">
-			<div className="num-display">{rating}</div>
-			<div className="text-display">{text}</div>
-			{/* <button onClick={handleClick}>Click</button> */}
+			<div className="num-display">{item.rating}</div>
+			<div className="text-display">{item.text}</div>
 		</div>
 	);
 }
