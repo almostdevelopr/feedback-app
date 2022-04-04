c3003274d5dfbd55505a85ea1adf2554a5969a60
almostdevelopr <minhalam.dev@gmail.com>
2022-04-04 16:25:10 +0530

    Create links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import { useState } from "react";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackStats from "./components/FeedbackStats";
+import AboutIconLink from "./components/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -15,14 +18,26 @@ function App() {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className="container">
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList feedback={feedback} />
+							</>
+						}
+					></Route>
+					<Route path="/about" element={<AboutPage />} />
+				</Routes>
+				<AboutIconLink />
 			</div>
-		</>
+		</Router>
 	);
 }
 export default App;
