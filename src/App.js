a03631ab94c2cfc0eaed8fc0f3536156f5ee9467
almostdevelopr <minhalam.dev@gmail.com>
2022-04-04 16:56:39 +0530

    Create context and provider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackForm from "./components/FeedbackForm";
 import FeedbackStats from "./components/FeedbackStats";
 import AboutIconLink from "./components/AboutIconLink";
 import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -18,26 +19,28 @@ function App() {
 	};
 
 	return (
-		<Router>
-			<Header />
-			<div className="container">
-				<Routes>
-					<Route
-						exact
-						path="/"
-						element={
-							<>
-								<FeedbackForm handleAdd={addFeedback} />
-								<FeedbackStats feedback={feedback} />
-								<FeedbackList feedback={feedback} />
-							</>
-						}
-					></Route>
-					<Route path="/about" element={<AboutPage />} />
-				</Routes>
-				<AboutIconLink />
-			</div>
-		</Router>
+		<FeedbackProvider>
+			<Router>
+				<Header />
+				<div className="container">
+					<Routes>
+						<Route
+							exact
+							path="/"
+							element={
+								<>
+									<FeedbackForm handleAdd={addFeedback} />
+									<FeedbackStats feedback={feedback} />
+									<FeedbackList feedback={feedback} />
+								</>
+							}
+						></Route>
+						<Route path="/about" element={<AboutPage />} />
+					</Routes>
+					<AboutIconLink />
+				</div>
+			</Router>
+		</FeedbackProvider>
 	);
 }
 export default App;
